d6d269f88589aacbaabb877722734bb272802022
2G2-99 <sagos0919@gmail.com>
2022-06-29 16:19:09 +0100

    JavaScript Standard Applied

--- Rock-Paper-Scissors_Project/Rock-Paper-Scissors_Project-script.js
+++ Rock-Paper-Scissors_Project/Rock-Paper-Scissors_Project-script.js
@@ -5,7 +5,7 @@ let computerScore = 0
 
 //
 // Computer Play
-function computerPlay() {
+function computerPlay () {
   const randomNumber = Math.floor(Math.random() * 3)
 
   switch (randomNumber) {
@@ -17,26 +17,26 @@ function computerPlay() {
       break
     case 2:
       return 'scissors'
-  };
-};
+  }
+}
 
 //
 // User Play
-function userPlay() {
-  let userInput = window.prompt('Rock, Paper or Scissors?', '').toLowerCase()
+function userPlay () {
+  let userInput = window.window.prompt('Rock, Paper or Scissors?', '').toLowerCase()
 
   if (userInput.includes('rock') || userInput.includes('paper') || userInput.includes('scissors')) {
     return (userInput)
   } else {
-    alert('Are sure?')
-    userInput = prompt('Rock, Paper or Scissors?').toLowerCase()
+    window.alert('Are sure?')
+    userInput = window.prompt('Rock, Paper or Scissors?').toLowerCase()
     return (userInput)
-  };
-};
+  }
+}
 
 //
 // Game Round
-function playRound(userSelection, computerSelection) {
+function playRound (userSelection, computerSelection) {
   if (userSelection === computerSelection) {
     round++
     console.log(`It's a tie, no one scores! and you are on round number ${round}`)
@@ -59,8 +59,8 @@ function playRound(userSelection, computerSelection) {
     computerScore++
     console.log(`You lost, ${computerSelection} beats ${userSelection}! and you are on round number ${round}`)
     console.log(`The score is ${userScore} - ${computerScore}`)
-  };
-};
+  }
+}
 
 //
 // Defining Parameters
@@ -70,15 +70,15 @@ const computerSelection = computerPlay()
 playRound(userSelection, computerSelection)
 //
 // Game Loop
-function game() {
+const game = function () {
   while (userScore < 5 && computerScore < 5) {
     playRound(userPlay(), computerPlay())
-  };
+  }
 
   if ((userScore === 5)) {
-    alert(`Congratulations! You win the game in round ${round}`)
+    window.alert(`Congratulations! You win the game in round ${round}`)
   } else if (computerScore === 5) {
-    alert(`GAME OVER! The CPU win the game in round ${round}`)
-  };
-};
+    window.alert(`GAME OVER! The CPU win the game in round ${round}`)
+  }
+}
 game()
