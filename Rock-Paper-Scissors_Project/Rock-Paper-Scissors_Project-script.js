ad2799fa7908131c352163f7ba55f1ecdb5165a9
2G2-99 <sagos0919@gmail.com>
2022-09-15 17:51:54 +0100

    Prettier

--- Rock-Paper-Scissors_Project/Rock-Paper-Scissors_Project-script.js
+++ Rock-Paper-Scissors_Project/Rock-Paper-Scissors_Project-script.js
@@ -1,84 +1,96 @@
 // Logic Variables
-let round = 0
-let userScore = 0
-let computerScore = 0
+let round = 0;
+let userScore = 0;
+let computerScore = 0;
 
 //
 // Computer Play
-function computerPlay () {
-  const randomNumber = Math.floor(Math.random() * 3)
+function computerPlay() {
+	const randomNumber = Math.floor(Math.random() * 3);
 
-  switch (randomNumber) {
-    case 0:
-      return 'rock'
-      break
-    case 1:
-      return 'paper'
-      break
-    case 2:
-      return 'scissors'
-  }
+	switch (randomNumber) {
+		case 0:
+			return 'rock';
+			break;
+		case 1:
+			return 'paper';
+			break;
+		case 2:
+			return 'scissors';
+	}
 }
 
 //
 // User Play
-function userPlay () {
-  let userInput = window.window.prompt('Rock, Paper or Scissors?', '').toLowerCase()
+function userPlay() {
+	let userInput = window.window
+		.prompt('Rock, Paper or Scissors?', '')
+		.toLowerCase();
 
-  if (userInput.includes('rock') || userInput.includes('paper') || userInput.includes('scissors')) {
-    return (userInput)
-  } else {
-    window.alert('Are sure?')
-    userInput = window.prompt('Rock, Paper or Scissors?').toLowerCase()
-    return (userInput)
-  }
+	if (
+		userInput.includes('rock') ||
+		userInput.includes('paper') ||
+		userInput.includes('scissors')
+	) {
+		return userInput;
+	} else {
+		window.alert('Are sure?');
+		userInput = window.prompt('Rock, Paper or Scissors?').toLowerCase();
+		return userInput;
+	}
 }
 
 //
 // Game Round
-function playRound (userSelection, computerSelection) {
-  if (userSelection === computerSelection) {
-    round++
-    console.log(`It's a tie, no one scores! and you are on round number ${round}`)
-    console.log(`The score is ${userScore} - ${computerScore}`)
-  } else if (
-    (userSelection === 'rock' && computerSelection === 'scissors') ||
-    (userSelection === 'paper' && computerSelection === 'rock') ||
-    (userSelection === 'scissors' && computerSelection === 'paper')
-  ) {
-    round++
-    userScore++
-    console.log(`You won, ${userSelection} beats ${computerSelection}! and you are on round number ${round}`)
-    console.log(`The score is ${userScore} - ${computerScore}`)
-  } else if (
-    (computerSelection === 'rock' && userSelection === 'scissors') ||
-    (computerSelection === 'paper' && userSelection === 'rock') ||
-    (computerSelection === 'scissors' && userSelection === 'paper')
-  ) {
-    round++
-    computerScore++
-    console.log(`You lost, ${computerSelection} beats ${userSelection}! and you are on round number ${round}`)
-    console.log(`The score is ${userScore} - ${computerScore}`)
-  }
+function playRound(userSelection, computerSelection) {
+	if (userSelection === computerSelection) {
+		round++;
+		console.log(
+			`It's a tie, no one scores! and you are on round number ${round}`
+		);
+		console.log(`The score is ${userScore} - ${computerScore}`);
+	} else if (
+		(userSelection === 'rock' && computerSelection === 'scissors') ||
+		(userSelection === 'paper' && computerSelection === 'rock') ||
+		(userSelection === 'scissors' && computerSelection === 'paper')
+	) {
+		round++;
+		userScore++;
+		console.log(
+			`You won, ${userSelection} beats ${computerSelection}! and you are on round number ${round}`
+		);
+		console.log(`The score is ${userScore} - ${computerScore}`);
+	} else if (
+		(computerSelection === 'rock' && userSelection === 'scissors') ||
+		(computerSelection === 'paper' && userSelection === 'rock') ||
+		(computerSelection === 'scissors' && userSelection === 'paper')
+	) {
+		round++;
+		computerScore++;
+		console.log(
+			`You lost, ${computerSelection} beats ${userSelection}! and you are on round number ${round}`
+		);
+		console.log(`The score is ${userScore} - ${computerScore}`);
+	}
 }
 
 //
 // Defining Parameters
-const userSelection = userPlay()
-const computerSelection = computerPlay()
+const userSelection = userPlay();
+const computerSelection = computerPlay();
 
-playRound(userSelection, computerSelection)
+playRound(userSelection, computerSelection);
 //
 // Game Loop
 const game = function () {
-  while (userScore < 5 && computerScore < 5) {
-    playRound(userPlay(), computerPlay())
-  }
+	while (userScore < 5 && computerScore < 5) {
+		playRound(userPlay(), computerPlay());
+	}
 
-  if ((userScore === 5)) {
-    window.alert(`Congratulations! You win the game in round ${round}`)
-  } else if (computerScore === 5) {
-    window.alert(`GAME OVER! The CPU win the game in round ${round}`)
-  }
-}
-game()
+	if (userScore === 5) {
+		window.alert(`Congratulations! You win the game in round ${round}`);
+	} else if (computerScore === 5) {
+		window.alert(`GAME OVER! The CPU win the game in round ${round}`);
+	}
+};
+game();
